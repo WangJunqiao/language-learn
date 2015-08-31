210257fdf89d5b08c322a919a4eb875eb371d88c
Junqiao Wang <junqiao.wang@hulu.com>
2015-08-31 21:11:08 +0800

    .

--- ruby/open-uri.rb
+++ ruby/open-uri.rb
@@ -4,17 +4,34 @@ require "open-uri"
 
 
 uri = "http://elssapi-smoke:8080/promo?query=lost"
-html_response = nil
 
-now = Time.now()
 
-open(uri) do |http|
-	html_response = http.read
-	puts http.status
-	puts http.status[0].to_i
+def get_url(uri)
+    puts "\n\nbegin #{uri}"
+    begin
+        now = Time.now()
+        open(uri) do |http|
+	          @html_response = http.read
+	          #puts http.status 这个会输出两行，一个200，一行OK
+	          puts http.status[0].to_i
+        end
+        puts Time.now() - now
+        #puts @html_response
+    rescue URI::InvalidURIError => e
+        puts e.message
+    rescue Errno::ECONNREFUSED => e
+        puts e.message
+    ensure
+        puts 'run ensure'
+    end
+    puts 'After begin/end'
 end
 
-puts Time.now() - now
+get_url('http://elssapi-smoke:8080/sapi/search/suggest?_device_id=1&_user_pgid=1&_content_pgid=29635&region=us&language=en&treatment=control&computerguid=BCCA608CF81844DFD613411CDCEDCDBF&userid=-1&his=&whis=&term=\u{fffd}\u{fffd}??\u{fffd}\u{fffd}\u{fffd}\u{fffd}\u{fffd}\u{fffd}\u{fffd}\u{fffd}?')
 
-puts html_response
+get_url('http://127.0.0.1:8080/xixihahaah')
+
+get_url('http://www.baidu.com')
+
+puts 'run here'
 
--- ruby/string.rb
+++ ruby/string.rb
@@ -1,10 +1,34 @@
 #! /usr/bin/ruby -w
-puts 'hello\nworld' #单引号为纯字符串，不考虑任何转义字符
-puts "hello\nworld" #考虑转义字符
 
-aaa = 'dd'
-puts %w(Hello,#{aaa}) #%w相当于单引号
-puts %W(Hello,#{aaa}) 
+@name = 'dd'
+
+puts 'hello\nworld' #单引号为纯字符串，不考虑任何转义字符
+puts "hello\nworld, #{@name}" #考虑转义字符和替换
+
+# %Q相当于双引号字符串，不同的是不需要太多的转义字符
+# (...)也可用其他非数字字母的符号或成对的符号代替, 诸如[...], !...!, +...+,{...}, <...>等.
+puts %Q("hello", #{@name}. 引号不需要转义,这两个字符\\\)还是要转义的！) 
+# %q跟%Q差不多，但是不进行替换
+puts %q("hello", #{@name}. 引号不需要转义,这两个字符\\\)还是要转义的！) 
+
+puts %q= haha jjjjj=
+puts %q&jjjjjj haha&
+puts %q'"hello world ---\''
+
+
+# 转化成数组，有替换效果
+puts %W("#{@name} bar)
+# 转化成数组，无替换效果
+puts %w("#{@name} bar)
+
+#执行shell语句，并返回结果
+puts %x(ls -alh)
+
+#正则表达式
+puts %r(/home/#{@name})
+
+#信号
+puts %s(foo ttt)
 
 
 str = 'test sub-string'
