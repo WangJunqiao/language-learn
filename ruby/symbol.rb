9d9658f65e2cddfa863669b147e044a41d3cbc21
Junqiao Wang <junqiao.wang@hulu.com>
2015-10-12 10:15:50 +0800

    ruby related update.

--- ruby/symbol.rb
+++ ruby/symbol.rb
@@ -1,5 +1,8 @@
 #! /usr/bin/ruby -w
 
+#不管字符串变量也好，符号变量也好，都是用来表示文本的，它们之间，也可以互相转换（通过to_sym和to_s）：
+#http://blog.csdn.net/besfanfei/article/details/7966987
+#相同的符号指向的对象是同一个，相同的字符串却不一定，所以映射一般是用符号的，不用字符串的
 def func(num, options={})
   puts "num = #{num}"
   options.each do |key, value|
--- ruby/test.rb
+++ ruby/test.rb
@@ -1,17 +1,13 @@
 #! /usr/bin/ruby -w
+env = "haha"
+db_conn = if env == :stg
+    "-h10.16.4.219 -uroot -pmojiti sapi"
+  else
+    '-hels.hamysql.prod.hulu.com -uhasql-sapi -psapi sapi'
+  end
 
-puts $*.size
-puts $*[0]
-
-@a = 0
-@b = 15
-
-while @a < 5 do
-    until @b > 20 do
-        puts "#@a, #@b"
-        @b += 1
-    end
-    
-    @a += 1
-end
+# IMPORTANT!!
+# if you change this table schema, remember also need to change update_autocomp_score.py FIELDS
+cmd = %q/echo 'ls'/
+puts `#{cmd}`
 
