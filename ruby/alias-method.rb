caa37ac1b11cef81dc00093d4177ffa06ce759af
Git Deploy <junqiao.wang@junqiao-mac.local>
2015-11-30 11:28:34 +0800

    update alias_method_chain example.

--- ruby/alias-method.rb
+++ ruby/alias-method.rb
@@ -1,30 +1,43 @@
+require 'active_support/all' #必须加上/all，不然没用
 
 class AAA
   def say
-    puts "I'm AAA instance."
+    puts "say"
   end
   alias_method :say_ya, :say #普通的函数别名功能
 end
-
 AAA.new.say_ya
 
 
 
 class BBB
-  def salute
-    puts "salute!!!"
+  def say
+    puts "say"
   end
 
-  def salute_with_log
-    puts "Calling method..."
-    salute_without_log
-    puts "...Method called"
+  def say_with_logging
+    puts "\nlogging BBB"
+    say_without_logging
   end
 
-  alias_method :salute_without_log, :salute
-  alias_method :salute, :salute_with_log
-  # alias_method_chain :salute, :log #这一句相当于上面的两句,但是需要activesupport，不知道为什么搞不起来
+  alias_method :say_without_logging, :say
+  alias_method :say, :say_with_logging
 end
 
-BBB.new.salute
-# 进过上面的已改动，外部调用salute的函数不用变，但是内部已经加了log功能！！！
+BBB.new.say
+
+
+class CCC
+  def say
+    puts "say"
+  end
+
+  def say_with_logging
+    puts "\nlogging, CCC"
+    say_without_logging
+  end
+
+  alias_method_chain :say, :logging
+end
+
+CCC.new.say
